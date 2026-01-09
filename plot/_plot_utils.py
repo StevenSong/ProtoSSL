946c78c6eec66a1f210fe23807cd5e79f7f28d5f
Steven Song <steven.song98@gmail.com>
2026-01-09 13:22:38 -0600

    plots

--- plot/_plot_utils.py
+++ plot/_plot_utils.py
@@ -1,9 +1,12 @@
 import os
+from typing import Literal
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import seaborn as sns
+import torch
+import yaml
 from matplotlib.patches import Circle, RegularPolygon
 from matplotlib.path import Path
 from matplotlib.projections import register_projection
@@ -11,6 +14,7 @@ from matplotlib.projections.polar import PolarAxes
 from matplotlib.spines import Spine
 from matplotlib.transforms import Affine2D
 from sklearn.metrics import auc, roc_curve
+from sklearn.metrics.pairwise import cosine_similarity
 
 MULTITASK_LABELS = [
     "LVEF Lo",
@@ -25,6 +29,13 @@ MULTITASK_LABELS = [
     "PASP Hi",
     "TRV Hi",
 ]
+
+
+PTBXL_GROUP_CSV_PATH = "/opt/gpudata/steven/ecg-prototype-transfer/external/bbj-lab-protoecgnet/scp_statementsRegrouped2.csv"
+ECHONEXT_GROUP_CSV_PATH = "/opt/gpudata/steven/ecg-prototype-transfer/external/bbj-lab-protoecgnet/echonext_label_groups.csv"
+ECHONEXT_ALIAS_PATH = "/opt/gpudata/steven/ecg-prototype-transfer/configs/targets.yaml"
+
+LABEL_SET_T = Literal["ptbxl", "echonext"]
 
 
 # Radar chart implementation from matplotlib example
@@ -194,6 +205,7 @@ def plot_radar(
     *,  # enforce kwargs
     title: str = "Multilabel AUROCs",
     legend_title: str | None = None,
+    save_path: str | None = None,
 ):
     experiments = {k: v for k, (v, c, l, m) in experiments_styles.items()}
     colors = [c for k, (v, c, l, m) in experiments_styles.items()]
@@ -236,6 +248,9 @@ def plot_radar(
     ax.set_rlim(0.40, 0.9)  # type: ignore
     ax.set_rgrids([0.5, 0.6, 0.7, 0.8, 0.9])  # type: ignore
     ax.legend(loc=(0.9, 0.95), title=legend_title)
+    if save_path is not None:
+        fig.tight_layout()
+        fig.savefig(save_path)
 
 
 # Create ROC curves for composite task
@@ -245,6 +260,7 @@ def plot_roc(
     *,  # enforce kwargs
     title: str = "Composite Task (SHD) ROC Curves",
     legend_title: str | None = None,
+    save_path: str | None = None,
 ):
     experiments = {k: v for k, (v, c, l, m) in experiments_styles.items()}
     colors = [c for k, (v, c, l, m) in experiments_styles.items()]
@@ -271,6 +287,9 @@ def plot_roc(
     ax.set_ylabel("True Positive Rate")
     ax.set_title(title)
     ax.legend(loc="lower right", title=legend_title)
+    if save_path is not None:
+        fig.tight_layout()
+        fig.savefig(save_path)
 
 
 def plot_lift(
@@ -280,6 +299,7 @@ def plot_lift(
     baseline_model: str,
     palette: dict[str, str],
     title: str,
+    save_path: str | None = None,
 ):
     palette = palette.copy()
     min_size = data["Train Size"].min()
@@ -315,3 +335,126 @@ def plot_lift(
     ax.set_xlim((min_size, max_size))
     ax.set_title(title)
     # ax.set_ylim((0.4, 0.9))
+    if save_path is not None:
+        fig.tight_layout()
+        fig.savefig(save_path)
+
+
+def get_labels(
+    *,  # enforce kwargs
+    label_set: LABEL_SET_T,
+    label_group: int,
+) -> list[str]:
+    if label_set == "ptbxl":
+        group_csv_path = PTBXL_GROUP_CSV_PATH
+    elif label_set == "echonext":
+        group_csv_path = ECHONEXT_GROUP_CSV_PATH
+    else:
+        raise ValueError(f"Bad label_set={label_set}")
+
+    label_groups = pd.read_csv(group_csv_path, index_col=0)
+    mask = label_groups["prototype_category"] == label_group
+    labels = label_groups[mask].index.to_list()
+    if label_set == "echonext":
+        with open(ECHONEXT_ALIAS_PATH, "r") as f:
+            aliases = yaml.safe_load(f)["target_columns"]
+            aliases = {v: k for k, v in aliases.items()}
+        labels = [aliases[x] for x in labels]
+    return labels
+
+
+def calc_ppc(
+    *,  # enforce kwargs
+    n_labels: int,
+    n_prototypes: int,
+    fallback_ppc: int = 5,
+) -> int:
+    if n_prototypes % n_labels != 0:
+        print(
+            f"Number of prototypes ({n_prototypes}) is not evenly divisble by number of labels ({n_labels})"
+        )
+        print(
+            f"If you are doing label-free projection from pretraining over a different label set, you can ignore this warning"
+        )
+        ppc = fallback_ppc
+    else:
+        ppc = n_prototypes // n_labels
+    return ppc
+
+
+def plot_prototype_similarity_heatmap(
+    *,  # enforce kwargs
+    ckpt_path: str,
+    title: str,
+    label_set: LABEL_SET_T,
+    label_group: int,
+    save_path: str | None = None,
+):
+    labels = get_labels(
+        label_set=label_set,
+        label_group=label_group,
+    )
+    sd = torch.load(ckpt_path, weights_only=False, map_location="cpu")
+    if "state_dict" in sd:
+        sd = sd["state_dict"]
+
+    # for 4d prototypes (from 2D partial or global convs)
+    prototypes = sd["model.prototype_vectors"]
+    prototypes = prototypes.view(prototypes.shape[0], -1)
+
+    sims = cosine_similarity(prototypes)
+    fig, ax = plt.subplots(figsize=(6, 5))
+    sns.heatmap(sims, ax=ax)
+    ax.set_title(f"{title} (Avg Cos Sim = {sims.mean():0.3f})")
+    ax.set_xlabel("Prototype Index")
+    ax.set_ylabel("Prototype Index")
+    n_prototypes = sims.shape[0]
+    n_labels = len(labels)
+    ppc = calc_ppc(n_labels=n_labels, n_prototypes=n_prototypes)
+    ticks = list(range(0, n_prototypes + 1, ppc))
+    ax.set_xticks(ticks=ticks, labels=ticks)  # type: ignore
+    ax.set_yticks(ticks=ticks)
+    ax.set_yticklabels(labels=ticks, rotation=0)  # type: ignore
+    if save_path is not None:
+        fig.tight_layout()
+        fig.savefig(save_path)
+
+
+def plot_prototype_label_indicators(
+    *,  # enforce kwargs
+    metadata_path: str,
+    title: str,
+    label_set: LABEL_SET_T,
+    label_group: int,
+    save_path: str | None = None,
+):
+    labels = get_labels(
+        label_set=label_set,
+        label_group=label_group,
+    )
+    metadata = pd.read_json(metadata_path).T
+    indicators = np.asarray(metadata["true_labels"].to_list())
+    n_prototypes, n_labels = indicators.shape
+    ppc = calc_ppc(n_labels=n_labels, n_prototypes=n_prototypes)
+    assert n_labels == len(
+        labels
+    ), "Number of provided labels does not match number of loaded classes in prototype metadata"
+    fig, ax = plt.subplots(figsize=(10, 5))
+    sns.heatmap(indicators.T, ax=ax, cbar=False, cmap="viridis")
+    ax.grid(axis="y")
+    xticks = list(range(0, n_prototypes + 1, ppc))
+    ax.set_xticks(ticks=xticks, labels=xticks)  # type: ignore
+    ax.set_yticks(range(0, n_labels + 1))
+    ax.set_yticks([x + 0.5 for x in range(0, n_labels)], minor=True)
+    ax.set_yticklabels(labels, minor=True)
+    ax.tick_params(
+        axis="y",
+        which="both",
+        length=0,
+    )
+    ax.set_xlabel("Prototype Index")
+    n_unique = metadata["ecg_id"].nunique()
+    ax.set_title(f"{title} (N Unique Samples = {n_unique})")
+    if save_path is not None:
+        fig.tight_layout()
+        fig.savefig(save_path)
