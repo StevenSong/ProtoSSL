9282f38c7b268c1ea5c07f9ff4d5a23d2019a319
Steven Song <songs1@uchicago.edu>
2026-01-09 13:33:01 -0600

    label-free projection transfer experiments (#5)
    * label free reprojection of pretrained, unprojected prototypes
    
    * set env var to ignore labels (used by modified push.py in protoecgnet)
    
    * bug fixes to job submit script
    
    * plots
    
    * ff subrepo

--- scripts/_slurm_wrapper.sh
+++ scripts/_slurm_wrapper.sh
@@ -4,4 +4,6 @@ source /opt/gpudata/steven/miniforge3/etc/profile.d/conda.sh
 conda activate ecg
 # echo WOULD RUN:
 # echo $1
+# echo "ECHONEXT_DATA=$ECHONEXT_DATA"
+# echo "RUN_DIR=$RUN_DIR"
 bash $1
