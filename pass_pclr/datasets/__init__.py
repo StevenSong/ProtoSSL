8dab8e409c28305b730ea0c5e39c80a42fd88546
Steven Song <steven.song98@gmail.com>
2026-02-11 13:59:30 -0600

    cinc dataset

--- pass_pclr/datasets/__init__.py
+++ pass_pclr/datasets/__init__.py
@@ -1,7 +1,8 @@
 from typing import Type
 
-from ..defines import ECHONEXT_TARGETS, PTBXL_TARGETS
+from ..defines import CINC_TARGETS, ECHONEXT_TARGETS, PTBXL_TARGETS
 from ._base_ecg_dataset import BaseECGDataset, StreamingECGWaveforms, load_cached_data
+from ._cinc_dataset import CincECGDataset
 from ._echonext_dataset import EchoNextECGDataset
 from ._heedb_dataset import HeedbECGDataset
 from ._pclr_wrapper_dataset import PCLRWrapperDataset
@@ -16,12 +17,15 @@ def infer_dataset_class_from_path(
 ]:
     echonext_indicators = ["echonext", "echo-next", "echo_next"]
     ptbxl_indicators = ["ptbxl", "ptb-xl", "ptb_xl"]
+    cinc_indicators = ["cinc", "cinc-2020", "cinc_2020", "cinc2020"]
     heedb_indicators = ["heedb"]
 
     if any(x in dataset_path for x in echonext_indicators):
         return EchoNextECGDataset, list(ECHONEXT_TARGETS.keys())
     elif any(x in dataset_path for x in ptbxl_indicators):
         return PtbxlECGDataset, PTBXL_TARGETS
+    elif any(x in dataset_path for x in cinc_indicators):
+        return CincECGDataset, CINC_TARGETS
     elif any(x in dataset_path for x in heedb_indicators):
         return HeedbECGDataset, None
     raise ValueError(
