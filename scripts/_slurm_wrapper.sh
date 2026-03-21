f897dddee9c08e765ec3d301aafd9af293f19ef2
Steven Song <songs1@uchicago.edu>
2026-03-21 08:11:23 -0500

    Fix Projections & Support for Distributed Projection and Predictions (#65)
    * address distributed prediction saving (probs and embeds)
    
    * distributed projection
    
    * have slurm wrapper prefix python with srun to seamlessly enable distributed
    
    * comment typo
    
    * cleanup plots

--- scripts/_slurm_wrapper.sh
+++ scripts/_slurm_wrapper.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# assumes submitting context has exported CONDA_PREFIX
-source "$CONDA_PREFIX/etc/profile.d/conda.sh"
+# assumes submitting context has exported CONDA_EXE
+source "$(dirname "$CONDA_EXE")/../etc/profile.d/conda.sh"
 conda activate ecg
 # echo WOULD RUN:
 # echo $1
 # echo "DATASET_PATH=$DATASET_PATH"
 # echo "RUN_DIR=$RUN_DIR"
+
+# Override python to use srun when submitted via this wrapper
+python() { srun python "$@"; }
+python3() { srun python3 "$@"; }
+export -f python
+export -f python3
+
 bash $1
