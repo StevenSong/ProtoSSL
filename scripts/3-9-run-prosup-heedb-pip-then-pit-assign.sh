782b0adf804d3d32b826363f8fc7c901f626b798
StevenSong <steven.song98@gmail.com>
2026-03-24 20:04:55 -0500

    parameterize prototypes per label when custom overriding

--- scripts/3-9-run-prosup-heedb-pip-then-pit-assign.sh
+++ scripts/3-9-run-prosup-heedb-pip-then-pit-assign.sh
@@ -14,6 +14,7 @@ cd $REPO_ROOT/scripts
 # experiment parameters
 EXP_NAME="prosup-heedb-pip-then-pit-assign"
 PRETRAIN_RUN="/opt/gpudata/steven/ecg-prototype-fm/outputs/prosup-pretrain-heedb"
+PPL=5
 
 # this version relies on learning prototype assignments relative to the target task
 python -m pass_pclr.trainer \
@@ -23,7 +24,7 @@ python -m pass_pclr.trainer \
     --trainer.logger.name $EXP_NAME \
     --data.dataset_path $DATASET_PATH \
     --model.pretrained_weights $PRETRAIN_RUN/project-prototypes-supervised/latest/proj.ckpt \
-    --model.n_prototypes_per_label 5 \
+    --model.n_prototypes_per_label $PPL \
     --model.n_prototypes 100  # from 5 per label for 20 heedb labels
 
 # then project
@@ -34,7 +35,7 @@ python -m pass_pclr.trainer \
     --trainer.logger.name $EXP_NAME \
     --data.dataset_path $DATASET_PATH \
     --model.pretrained_weights $RUN_DIR/$EXP_NAME/learn-prototype-assignments/latest/assigned.ckpt \
-    --model.n_prototypes_per_label 5 \
+    --model.n_prototypes_per_label $PPL \
     --model.n_prototypes null
 
 # then train classifier
@@ -45,7 +46,7 @@ python -m pass_pclr.trainer \
     --trainer.logger.name $EXP_NAME \
     --data.dataset_path $DATASET_PATH \
     --model.pretrained_weights $RUN_DIR/$EXP_NAME/project-prototypes-supervised/latest/proj.ckpt \
-    --model.n_prototypes_per_label 5 \
+    --model.n_prototypes_per_label $PPL \
     --model.n_prototypes null
 
 python _eval_probs.py \
