57d2b00875617ea41f0b8d79b7530e32cfcac1ac
StevenSong <steven.song98@gmail.com>
2026-03-26 21:28:56 -0500

    forgot to add env var exports

--- scripts/queue-experiments.sh
+++ scripts/queue-experiments.sh
@@ -3,15 +3,31 @@
 set -e
 
 declare -a DATASETS=("echonext" "ptbxl" "cinc" "mimic" "zzu")
+declare -A DATASET_DIRS=(
+    ["echonext"]="echonext"
+    ["ptbxl"]="ptb-xl"
+    ["cinc"]="cinc-2020"
+    ["mimic"]="mimic-iv-ecg"
+    ["zzu"]="zzu-pecg"
+)
 declare -a SUFFIXES_echonext=("" "-32k" "-16k" "-8k" "-4k" "-2k" "-1k" "-512" "-256")
 declare -a SUFFIXES_ptbxl=("" "-8k" "-4k" "-2k" "-1k" "-512" "-256")
 declare -a SUFFIXES_cinc=("" "-4k" "-2k" "-1k" "-512" "-256")
 declare -a SUFFIXES_mimic=("" "-32k" "-16k" "-8k" "-4k" "-2k" "-1k" "-512" "-256")
 declare -a SUFFIXES_zzu=("" "-4k" "-2k" "-1k" "-512" "-256")
 
+export BASE_ECG_PATH=/opt/gpudata/ecg
+export REPO_ROOT=/opt/gpudata/steven/ecg-prototype-fm
+export BASE_OUTPUT_DIR=/opt/gpudata/steven/ecg-prototype-fm/outputs
+
+source _submit_job.sh
+
 
 for dataset in "${DATASETS[@]}"; do
     echo "DATASET: $dataset"
+    ds_dir="${DATASET_DIRS[$dataset]}"
+    export BASE_RUN_DIR="$BASE_OUTPUT_DIR/runs-$dataset"
+    export BASE_DATASET_PATH="$BASE_ECG_PATH/$ds_dir"
 
     array_name="SUFFIXES_${dataset}[@]"
     suffixes=("${!array_name}")
