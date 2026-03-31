703f94497f4e278a10953badfb8ed695d6ea0c48
StevenSong <steven.song98@gmail.com>
2026-03-31 13:26:31 -0500

    reorg scripts

--- scripts/queue-experiments.sh
+++ scripts/queue-experiments.sh
@@ -37,16 +37,13 @@ for dataset in "${DATASETS[@]}"; do
 
         cache_id=$(submit_job "$suffix" 0-run-cache-data.sh)
         echo $cache_id
-        submit_job "$suffix" 1-run-blackbox-direct.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 2-01-run-labsup-proto-direct-5ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 2-02-run-labsup-proto-direct-14ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 3-01-run-protossl-heedb-pia-5ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 3-02-run-protossl-heedb-pia-14ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 3-03-run-protossl-no-attn-heedb-pia-5ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 3-04-run-protossl-no-attn-heedb-pia-14ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 4-01-run-labsup-proto-heedb-ria-5ppl.sh "--dependency=afterok:$cache_id"
-        submit_job "$suffix" 4-02-run-labsup-proto-heedb-ria-14ppl.sh "--dependency=afterok:$cache_id"
+        # submit_job "$suffix" 1-run-blackbox-direct.sh "--dependency=afterok:$cache_id"
+        # submit_job "$suffix" 2-run-labsup-proto-direct-14ppl.sh "--dependency=afterok:$cache_id"
+        # submit_job "$suffix" 3-01-run-protossl-no-attn-heedb-pia-14ppl.sh "--dependency=afterok:$cache_id"
+        submit_job "$suffix" 3-02-run-protossl-no-attn-heedb-pila-14ppl.sh "--dependency=afterok:$cache_id"
+        # submit_job "$suffix" 4-01-run-labsup-proto-heedb-ria-14ppl.sh "--dependency=afterok:$cache_id"
+        submit_job "$suffix" 4-02-run-labsup-proto-heedb-rila-14ppl.sh "--dependency=afterok:$cache_id"
 
-        submit_job "$suffix" 5-1-run-ecgfounder-logreg.sh # does not depend on same 100 Hz cache (takes 500 Hz)
+        # submit_job "$suffix" 5-1-run-ecgfounder-logreg.sh # does not depend on same 100 Hz cache (takes 500 Hz)
     done
 done
