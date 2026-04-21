bc152b1d142296766a75107554f5e7fce719153f
StevenSong <steven.song98@gmail.com>
2026-04-21 16:59:51 -0500

    resolve torchcodec dependencies for datasets (bump torch to 2.7, tested), implement speech commands dataset

---
+++ protossl/datasets/_speech_commands_dataset.py
@@ -0,0 +1,67 @@
+import numpy as np
+import pandas as pd
+import torch
+from datasets import Dataset as HFDataset
+from datasets import load_dataset
+
+from ..defines import SPEECH_COMMANDS_V2_TARGETS, SPLIT_T
+from ._base_dataset import BaseTSDataset
+from .streaming_loaders import StreamingAudioWaveforms
+
+
+class SpeechCommandsV2Dataset(BaseTSDataset):
+    def __init__(
+        self,
+        *,
+        dataset_path: str,
+        split: SPLIT_T,
+        sampling_rate: int,
+        label_subset: list[str] | None = None,
+    ):
+        """
+        We use the huggingface version of this dataset. At the time of writing,
+        the default implementation of the dataset still uses a legacy script to
+        load the data. This is no longer supported since datasets>=4.0.0, which
+        we use. There is however a PR to convert to the standard format:
+        https://huggingface.co/datasets/google/speech_commands/discussions/5
+
+        We download this revision and store locally using the following command:
+        ```
+        hf download --type dataset google/speech_commands --revision 41990749 --local-dir /path/to/local/speech-commands-v2
+        ```
+        """
+        if label_subset is not None:
+            raise NotImplementedError(
+                f"label_subset not yet supported for {type(self.__name__)}"
+            )
+
+        _split = split
+        if split == "val":
+            _split = "validation"
+        hf_ds: HFDataset = load_dataset(dataset_path, name="v0.02", split=_split)  # type: ignore
+        pids, pid_to_id = pd.factorize(np.asarray(hf_ds["speaker_id"]))
+
+        self.source_ids = torch.as_tensor(pids, dtype=torch.long)
+        self.sample_ids = torch.arange(len(hf_ds), dtype=torch.long)
+
+        label_to_idx = {k: i for i, k in enumerate(SPEECH_COMMANDS_V2_TARGETS)}
+        hf_idx_to_label = {i: k for i, k in enumerate(hf_ds.features["label"].names)}
+
+        y = torch.zeros((len(hf_ds), len(SPEECH_COMMANDS_V2_TARGETS)), dtype=torch.long)
+        for i, hf_idx in enumerate(hf_ds["label"]):
+            label = hf_idx_to_label[hf_idx]
+            if label not in label_to_idx:
+                continue
+            idx = label_to_idx[label]
+            y[i, idx] = 1
+        self.labels = y
+
+        self.waveforms = StreamingAudioWaveforms(
+            hf_ds=hf_ds,
+            sampling_rate=sampling_rate,
+            clip_seconds=10.0,
+        )
+
+        assert self.source_ids.shape[0] == self.waveforms.shape[0]
+        assert self.source_ids.shape[0] == self.sample_ids.shape[0]
+        assert self.source_ids.shape[0] == self.labels.shape[0]
--- protossl/datasets/streaming_loaders/_streaming_audio.py
+++ protossl/datasets/streaming_loaders/_streaming_audio.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import torch
 import torch.nn.functional as F
 import torchaudio
+from datasets import Dataset as HFDataset
+from datasets.features import Audio
 from scipy.signal import resample_poly
 
 from ._streaming_base import StreamingWaveformsBase
@@ -13,11 +15,22 @@ class StreamingAudioWaveforms(StreamingWaveformsBase):
     def __init__(
         self,
         *,  # enforce kwargs
-        wav_paths: list[Path],
+        wav_paths: list[Path] | None = None,
+        hf_ds: HFDataset | None = None,
         sampling_rate: int,
         clip_seconds: float = 10.0,
     ):
         self.wav_paths = wav_paths
+        self.hf_ds = hf_ds
+        if wav_paths is None and hf_ds is None:
+            raise ValueError(
+                "Must provide one of either wav_paths or hf_ds, both were None"
+            )
+        if hf_ds is not None:
+            if "audio" not in hf_ds.features:
+                raise ValueError("'audio' feature not found in HF dataset")
+            if not isinstance(hf_ds.features["audio"], Audio):
+                raise ValueError("'audio' feature not of expected type")
         self.sampling_rate = sampling_rate
         self.clip_seconds = clip_seconds
         self.target_len = int(sampling_rate * clip_seconds)
@@ -26,8 +39,15 @@ class StreamingAudioWaveforms(StreamingWaveformsBase):
         print("=======================================================")
 
     def __getitem__(self, i: int) -> torch.Tensor:
-        fpath = self.wav_paths[i]
-        x, source_sr = torchaudio.load(str(fpath))  # (C, T)
+        if self.wav_paths is not None:
+            fpath = self.wav_paths[i]
+            x, source_sr = torchaudio.load(str(fpath))  # (C, T)
+        elif self.hf_ds is not None:
+            sample = self.hf_ds[i]["audio"].get_all_samples()
+            x = sample.data  # (C, T)
+            source_sr = sample.sample_rate
+        else:
+            raise ValueError("Unknown how to get sample")
 
         # convert to mono
         if x.shape[0] > 1:
@@ -50,7 +70,7 @@ class StreamingAudioWaveforms(StreamingWaveformsBase):
         else:
             x = x.float()
 
-        # crop or pad to fixed 10-second length
+        # crop or pad to fixed length
         cur_len = x.shape[1]
         if cur_len > self.target_len:
             x = x[:, : self.target_len]
@@ -66,4 +86,10 @@ class StreamingAudioWaveforms(StreamingWaveformsBase):
 
     @property
     def shape(self) -> tuple[int, ...]:
-        return (len(self.wav_paths), 1, self.target_len)
+        if self.wav_paths is not None:
+            n = len(self.wav_paths)
+        elif self.hf_ds is not None:
+            n = len(self.hf_ds)
+        else:
+            raise ValueError("Unknown how to get shape")
+        return (n, 1, self.target_len)
