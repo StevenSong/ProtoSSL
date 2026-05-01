3c18aa4d369906f65c5ea310e00bdba7e6fdde21
StevenSong <steven.song98@gmail.com>
2026-05-01 09:20:36 -0500

    add fine tuning to protopool

--- scripts/6-run-protossl-heedb-pia.sh
+++ scripts/6-run-protossl-heedb-pia.sh
@@ -61,3 +61,51 @@ python _eval_probs_bootstrapped.py \
 --dataset-path $DATASET_PATH \
 --probs-npy $RUN_DIR/$EXP_NAME/probs.npy \
 --output-path $RUN_DIR/$EXP_NAME
+
+# now fine-tune
+PRETRAIN_RUN=$RUN_DIR/$EXP_NAME
+EXP_NAME="$EXP_NAME-ft"
+
+python -m protossl.trainer \
+    --config $REPO_ROOT/configs/target-guided-14ppl.yaml \
+    --seed_everything $SEED \
+    --pipeline-stage learn-prototypes-supervised \
+    --trainer.logger.save_dir $RUN_DIR/ \
+    --trainer.logger.name $EXP_NAME \
+    --data.dataset_path $DATASET_PATH \
+    --model.pretrained_weights $PRETRAIN_RUN/learn-prototype-assignments/latest/assigned.ckpt
+
+python -m protossl.trainer \
+    --config $REPO_ROOT/configs/target-guided-14ppl.yaml \
+    --seed_everything $SEED \
+    --pipeline-stage project-prototypes-supervised \
+    --trainer.logger.save_dir $RUN_DIR/ \
+    --trainer.logger.name $EXP_NAME \
+    --data.dataset_path $DATASET_PATH \
+    --model.pretrained_weights $RUN_DIR/$EXP_NAME/learn-prototypes-supervised/latest/best.ckpt
+
+python -m protossl.trainer \
+    --config $REPO_ROOT/configs/target-guided-14ppl.yaml \
+    --seed_everything $SEED \
+    --pipeline-stage compute-embeddings \
+    --trainer.logger.save_dir $RUN_DIR \
+    --trainer.logger.name $EXP_NAME \
+    --data.dataset_path $DATASET_PATH \
+    --model.pretrained_weights $RUN_DIR/$EXP_NAME/project-prototypes-supervised/latest/proj.ckpt
+
+python _linear_probe.py \
+--random-seed $SEED \
+--dataset-path $DATASET_PATH \
+--prototype-embeddings $RUN_DIR/$EXP_NAME/compute-embeddings/latest \
+--output-path $RUN_DIR/$EXP_NAME
+
+python _eval_probs.py \
+--dataset-path $DATASET_PATH \
+--probs-npy $RUN_DIR/$EXP_NAME/probs.npy \
+--output-path $RUN_DIR/$EXP_NAME
+
+python _eval_probs_bootstrapped.py \
+--dataset-path $DATASET_PATH \
+--probs-npy $RUN_DIR/$EXP_NAME/probs.npy \
+--output-path $RUN_DIR/$EXP_NAME
+
